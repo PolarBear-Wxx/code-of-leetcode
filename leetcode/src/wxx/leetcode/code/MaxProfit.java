102d18f0cf5b99324a86f27cbe6667d95cccc515
Wxx <polar_bear_brother@163.com>
2019-10-23 16:05:50 +0800

    add others() of easy-121

--- leetcode/src/wxx/leetcode/code/MaxProfit.java
+++ leetcode/src/wxx/leetcode/code/MaxProfit.java
@@ -11,7 +11,7 @@ public class MaxProfit{
      * @param prices
      * @return
      */
-    public static int maxProfit(int[] prices){
+    public static int mine(int[] prices){
         int mp = 0;
         for(int i = 0; i < prices.length; i++){
             for(int j = 0; j < i; j++){
@@ -23,9 +23,25 @@ public class MaxProfit{
         }
         return mp;
     }
+    /**
+     * from discuss, faster than mine
+     * Here, the logic is to calculate the difference (maxCur += prices[i] - prices[i-1]) of the original array, 
+     * and find a contiguous subarray giving maximum profit. If the difference falls below 0, reset it to zero.
+     * @param prices
+     * @return
+     */
+    public static int others(int[] prices){
+        int maxCur = 0, maxSofar = 0;
+        for(int i = 1; i < prices.length; i++){
+            maxCur = Math.max(0, maxCur += prices[i] - prices[i-1]);
+            maxSofar = Math.max(maxCur, maxSofar);
+        }
+        return maxSofar;
+    }
     public static void main(String[] args) {
         int[] test = {1,2,3,6,2};
         System.out.println("input: " + test);
-        System.out.println("output: " + MaxProfit.maxProfit(test));
+        System.out.println("output: " + MaxProfit.mine(test));
+        System.out.println("output: " + MaxProfit.others(test));
     }
 }
