bfe63185ed428a410309c0f7a53b6e4d96f1377e
Wxx <polar_bear_brother@163.com>
2019-10-25 20:23:58 +0800

    add my_non_recursive() for easy-101 add toBTree(int[] array) in BTNode.java a unresolved compilation problem!!!

--- leetcode/src/wxx/leetcode/code/BTNode.java
+++ leetcode/src/wxx/leetcode/code/BTNode.java
@@ -1,13 +1,16 @@
 package wxx.leetcode.code;
 
+import java.util.Queue;
+import java.util.Stack;
+
 /**
  * binary tree node
- * only use in this package
+ * only use in this package -- 2019-10-25修改为public类
  * @version 1.01 2019-10-24
  * @author Khada Jhin
  */
 
-class BTNode{
+public class BTNode{
     int val;
     BTNode left;
     BTNode right;
@@ -22,4 +25,54 @@ class BTNode{
     public static void test(){
         System.out.println("BTNode import successfully!");
     }
+    /**
+     * convert a integer array to a binary tree.
+     * "-1" indicates a empty node in the integer array!!!
+     * @version 1.01 2019-10-25
+     * @author Khada Jhin 
+     */
+    public static BTNode toBTree(int[] array){
+        if(array[0]==-1){
+            System.out.println("warning: your input is an empty tree!");
+            return null;   // "-1" indicates a empty node in the integer array!!! 
+        }
+        BTNode root = new BTNode(array[0]);
+        int index = 1;
+        //Stack<BTNode> stack = new Stack<BTNode>();
+        //stack.push(root);
+        Queue<BTNode> queue = new ArrayBlockingQueue<BTNode>();
+        queue.add(root);
+        BTNode top,temp;
+        while(queue.isEmpty()==false && index<array.length){
+            top = queue.poll();
+            if(array[index]!=-1){
+                temp = new BTNode(array[index]);
+                top.left = temp;
+                queue.add(temp);
+                index += 1;
+            }
+            else{
+                top.left = null;
+                index += 1;
+            }
+            if(array[index]!=-1){
+                temp = new BTNode(array[index]);
+                top.right = temp;
+                queue.add(temp);
+                index += 1;
+            }
+            else{
+                top.right = null;
+                index += 1;
+            }
+        }
+        return root;
+    }
+    /**
+     * convert a string to a binary tree
+     * establishe on 2019-10-25
+     * @version 1.01 accomphlished on ???
+     * @author Khada Jhin
+     */
+    //public static BTNode toBTree(String s){}
 }
--- leetcode/src/wxx/leetcode/code/Easy101.java
+++ leetcode/src/wxx/leetcode/code/Easy101.java
@@ -1,5 +1,8 @@
 package wxx.leetcode.code;
 
+import java.util.Arrays;
+import java.util.Stack;
+
 /**
  * easy-101 :check whether a binary tree is a mirror of itself
  * @version 1.01 2019-10-24 Happy programmer's day!
@@ -7,6 +10,11 @@ package wxx.leetcode.code;
  */
 
 public class Easy101{
+    /**
+     * my recursive solution, it can't solve this problem correctly!!!
+     * @param root
+     * @return
+     */
     public static boolean mine(BTNode root){
         //System.out.println("I have no idea!");
         if(root == null)
@@ -21,13 +29,60 @@ public class Easy101{
             return false;
         return (left.val == right.val) && sh(left.left, left.right) && sh(right.left,right.right) && sh(left.right, right.left);
     }
+    /**
+     * my non-recursive solution
+     * @param root
+     * @return
+     */
+    public static boolean my_non_recursive(BTNode root){
+        if(root == null)
+            return true;
+        Stack<BTNode> stack = new Stack<BTNode>();
+        if(root.left != null){
+            if(root.right == null)
+                return false;
+            else{
+                stack.push(root.left);
+                stack.push(root.right);
+            }
+        }
+        else if(root.right!=null)
+            return false;
+        BTNode left, right;
+        while(!stack.empty()==true){
+            //if(stack.size()%2!=0)  return false;  这行代码有必要吗？
+            //good indentation habit : 小括号里的运算符就不要空格了
+            right = stack.pop();
+            left = stack.pop();
+            if(left.val!=right.val) return false;
+            if(left.left!=null){
+                if(right.right==null) return false;
+                stack.push(left.left);
+                stack.push(right.right);
+            }
+            else if(right.right!=null) return false;
+            if(left.right!=null){
+                if(right.left==null)  return false;
+                stack.push(left.right);
+                stack.push(right.left);
+            }
+            else if(right.left==null)  return false;
+        }
+        return true;
+    }
+    /**
+     * copy from discuss 
+     * recursive solution
+     * @param root
+     * @return
+     */
     public static boolean others(BTNode root){
         System.out.println("copy from disscuss");
         return root==null || isSymmetricHelp(root.left, root.right);
     }
     private static boolean isSymmetricHelp(BTNode left, BTNode right){
         if(left==null || right==null)
-            return left==right;
+            return left==right; //null 也可以进行“==”运算
         if(left.val!=right.val)
             return false;
         return isSymmetricHelp(left.left, right.right) && isSymmetricHelp(left.right, right.left);
@@ -39,6 +94,7 @@ public class Easy101{
          *BTNode.test();
          *System.out.println("very good!");
          */
+        /* 
         BTNode node4 = new BTNode(3, null, null);
         BTNode node5 = new BTNode(4, null, null);
         BTNode node6 = new BTNode(4, null, null);
@@ -46,10 +102,13 @@ public class Easy101{
         BTNode node2 = new BTNode(2, node4, node5);
         BTNode node3 = new BTNode(2, node6, node7);
         BTNode root = new BTNode(1, node2, node3);
-        if(Easy101.mine(root) == true){
-            System.out.println("[" + root.val + "," + node2.val + "," + node3.val + "] is a symmetric tree.");
+        */
+        int[] data ={1,2,2,3,4,4,3};
+        BTNode root = BTNode.toBTree(data);
+        if(Easy101.my_non_recursive(root) == true){
+            System.out.println("input: " + Arrays.toString(data) + " is a symmetric tree.");
         }
         else
-            System.out.println("[" + root.val + "," + node2.val + "," + node3.val + "] is not a symmetric tree.");
+            System.out.println("input: " + Arrays.toString(data) + " is not a symmetric tree.");
     }
 }
