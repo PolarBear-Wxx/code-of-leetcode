076417f1df22e7d91b5320bc7b925e1c51a57059
Wxx <polar_bear_brother@163.com>
2019-10-25 09:22:19 +0800

    add my recursive solution for easy-101 even though it is wrong.

--- leetcode/src/wxx/leetcode/code/Easy101.java
+++ leetcode/src/wxx/leetcode/code/Easy101.java
@@ -8,8 +8,18 @@ package wxx.leetcode.code;
 
 public class Easy101{
     public static boolean mine(BTNode root){
-        System.out.println("I have no idea!");
-        return false;
+        //System.out.println("I have no idea!");
+        if(root == null)
+            return true;
+        else
+            return sh(root.left, root.right);
+    }
+    public static boolean sh(BTNode left, BTNode right){
+        if(left == null && right == null)
+            return true;
+        else if((left == null && right != null) || (left != null && right == null))
+            return false;
+        return (left.val == right.val) && sh(left.left, left.right) && sh(right.left,right.right) && sh(left.right, right.left);
     }
     public static boolean others(BTNode root){
         System.out.println("copy from disscuss");
@@ -29,10 +39,14 @@ public class Easy101{
          *BTNode.test();
          *System.out.println("very good!");
          */
-        BTNode node2 = new BTNode(2, null, null);
-        BTNode node3 = new BTNode(2, null, null);
+        BTNode node4 = new BTNode(3, null, null);
+        BTNode node5 = new BTNode(4, null, null);
+        BTNode node6 = new BTNode(4, null, null);
+        BTNode node7 = new BTNode(3, null, null);
+        BTNode node2 = new BTNode(2, node4, node5);
+        BTNode node3 = new BTNode(2, node6, node7);
         BTNode root = new BTNode(1, node2, node3);
-        if(Easy101.others(root) == true){
+        if(Easy101.mine(root) == true){
             System.out.println("[" + root.val + "," + node2.val + "," + node3.val + "] is a symmetric tree.");
         }
         else
