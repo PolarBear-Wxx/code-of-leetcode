c354ba90cccd54da0ffd6325af54a2d2a7e438a7
Wxx <polar_bear_brother@163.com>
2019-10-31 13:53:54 +0800

    I am very tired

---
+++ leetcode/src/wxx/leetcode/code/BTLevelOrderTraversal.java
@@ -0,0 +1,24 @@
+package wxx.leetcode.code;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+/**
+ * easy-107 
+ * @version 1.01 2019-10-31
+ * @author Khada Jhin
+ */
+
+public class BTLevelOrderTraversal{
+    public static List<List<Integer>> mine(BTNode root){
+        Queue<BTNode> queue = new LinkedList<BTNode>();
+        List<List<Integer>> wrapList = new LinkedList<List<Integer>>();
+        System.out.println("先搞明白List接口和LinkedList类！！！");
+        return null;
+    }
+    public static void main(String[] args) {
+        BTNode root = new BTNode(999);
+        mine(root);
+    }
+}
