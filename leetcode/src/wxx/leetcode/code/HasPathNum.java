1075efb306882d795e410cd1377ebd8a73615102
Wxx <polar_bear_brother@163.com>
2019-11-04 00:31:18 +0800

    add HasPathNum.java and the mine() is true only all the value of nodes is non-negative!

---
+++ leetcode/src/wxx/leetcode/code/HasPathNum.java
@@ -0,0 +1,47 @@
+package wxx.leetcode.code;
+
+import java.util.LinkedList;
+
+/**
+ * easy-112
+ * 
+ * @version 1.01 2019-11-3
+ * @author Khada Jhin
+ */
+
+public class HasPathNum{
+    public static boolean mine(BTNode root, int sum){
+        if(root == null || root.val > sum) return false;
+        LinkedList<BTNode> list = new LinkedList<BTNode>();
+        LinkedList<Integer> current_path_sum = new LinkedList<Integer>();
+        list.offer(root);
+        current_path_sum.offer(root.val);
+        BTNode curreBtNode;
+        int current_sum;
+        while(list.isEmpty() == false ){
+            curreBtNode = list.poll();
+            current_sum = current_path_sum.poll();
+            if(current_sum == sum && curreBtNode.left == null && curreBtNode.right == null) return true;
+            if(curreBtNode.left != null && curreBtNode.left.val + current_sum <= sum){
+                list.offer(curreBtNode.left);
+                current_path_sum.offer(curreBtNode.left.val + current_sum);
+                if(curreBtNode.right != null && curreBtNode.right.val + current_sum <= sum){
+                    list.offer(curreBtNode.right);
+                    current_path_sum.offer(curreBtNode.right.val + current_sum);
+                }
+            }
+            else if(curreBtNode.right != null && curreBtNode.right.val + current_sum <= sum){
+                list.offer(curreBtNode.right);
+                current_path_sum.offer(curreBtNode.right.val + current_sum);
+            }
+
+        }
+        return false;
+    }
+    public static void main(String[] args) {
+        int[] test = {5,4,8,11,-1,13,4,7,2,-1,-1,-1,1};
+        BTNode ttest = BTNode.toBTree(test);
+        System.out.println(ttest);
+        System.out.println(mine(ttest, 22));
+    }
+}
