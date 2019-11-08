65137600af505fad02eaa5909c5dd1b2f8fb312d
Wxx <polar_bear_brother@163.com>
2019-11-08 10:44:49 +0800

    easy-160, easy-167

--- leetcode/src/wxx/leetcode/code/Solution.java
+++ leetcode/src/wxx/leetcode/code/Solution.java
@@ -17,6 +17,7 @@ class ListNode{
     }
 }
 public class Solution{
+
     /**
      * easy-141 Linked List Cycle
      */
@@ -31,5 +32,63 @@ public class Solution{
             help.add(curNode);
         }
         return false;
+    }
+
+    /**
+     * easy-160 Intersection of Two Linked List
+     */
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        if(headA == null || headB == null) return null;
+        ListNode a_cur = headA;
+        ListNode b_cur = headB;
+        while(a_cur != null){
+            while(b_cur != null){
+                if(a_cur == b_cur) return a_cur;
+                else b_cur = b_cur.next;
+            }
+            b_cur = headB;
+            a_cur = a_cur.next;
+        }
+        return null;
+    }
+    /**
+     * easy-160 a smart solution from discuss
+     * You can prove that: say A length = a + c, B length = b + c, after switching pointer, 
+     * pointer A will move another b + c steps, pointer B will move a + c more steps, 
+     * since a + c + b + c = b + c + a + c, it does not matter what value c is. 
+     * Pointer A and B must meet after a + c + b (b + c + a) steps. If c == 0, they meet at NULL.
+     * Thanks, hpplayer. This solution is very smart.
+     */
+    public ListNode getIntersectionNode_others(ListNode headA, ListNode headB) {
+        //boundary check
+        if(headA == null || headB == null) return null;
+        
+        ListNode a = headA;
+        ListNode b = headB;
+        
+        //if a & b have different len, then we will stop the loop after second iteration
+        while( a != b){
+            //for the end of first iteration, we just reset the pointer to the head of another linkedlist
+            a = a == null? headB : a.next;
+            b = b == null? headA : b.next;    
+        }
+        
+        return a;
+    }
+
+    /**
+     * easy-167 Two Sum II - input array is sorted
+     */
+    public int[] twoSum(int[] numbers, int target) {
+        int[] result = new int[2];
+        for(int i = 0; i < numbers.length; i++){
+            for(int j = i + 1; j < numbers.length; j++){
+                if(numbers[i] + numbers[j] == target){
+                    result[0] = i;
+                    result[1] = j;
+                }
+            }
+        }
+        return result;
     }
 }
